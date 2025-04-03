158bd7336d87c8114b4169eee3475003bb504cf2
Malihashahi <maliha.shahi.03@gmail.com>
2025-04-03 22:27:22 +0430

    add template folder and create first tempelt with django

--- accounts_app/views.py
+++ accounts_app/views.py
@@ -1,18 +1,7 @@
 from django.shortcuts import render , HttpResponse
-users = ['bahar', 'hadid' , 'nergis','sam']
-blocked_users = ['nergis' ,'hadid']
+
 
 def profile(request , username):
-    if username in users:
-         if username in blocked_users:
-             return HttpResponse(f"{username} is blocked")
-         else:
-            return HttpResponse(f"this is {username} s profile")
-    else:
-        return HttpResponse('This username is not register')
-
-
-
-
+      return  render(request ,"accounts_app/profile.html")
 def codeyade(request):
     return HttpResponse("this is  codeyade webseite")
--- blog_app/urls.py
+++ blog_app/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
      path('home', views.home),
      path('contactus', views.contactus)
+     
 ]
