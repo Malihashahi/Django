2d9e17bb052f41ef28ad178618f027fed111be9e
Malihashahi <maliha.shahi.03@gmail.com>
2025-04-07 23:06:20 +0430

    apply django template on multi user list

--- accounts_app/views.py
+++ accounts_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render , HttpResponse
+from django.http import Http404
 users = [
 { 
      'username' :'bahar',
@@ -6,11 +7,35 @@ users = [
      'lastname': 'shahi',
      'phone' : '093883883'
 
+},
+{ 
+     'username' :'bahar',
+     'name' : 'malihabahar',
+     'lastname': 'shahi',
+     'phone' : '093883883'
+
+},
+{ 
+     'username' :'hadid',
+     'name' : 'sediqa',
+     'lastname': 'golobi',
+     'phone' : '09388883883'
+
+},{ 
+     'username' :'sam',
+     'name' : 'samirbayan',
+     'lastname': 'bayan',
+     'phone' : '07990003883'
+
 }
+
+
 ]
 
 def profile(request , username):
-      print(username)
-      return  render(request ,"accounts_app/profile.html", context={"name": username})
+    for user in users:
+         if user['username'] == username:
+            return render( request , 'accounts_app/profile.html',context={'user': user})
+    raise Http404('this user is not found')
 def info(request):
     return render(request  ,"accounts_app/info.html")
