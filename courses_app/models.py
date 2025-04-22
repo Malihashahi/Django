e665dd1962aef1aa9f9fb808488a372d5b88a1d1
Malihashahi <maliha.shahi.03@gmail.com>
2025-04-22 22:47:22 +0430

    register coures models on adim panel

--- courses_app/models.py
+++ courses_app/models.py
@@ -4,3 +4,7 @@ from django.db import models
 class Course(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField()
+
+
+    def __str__(self):
+        return self.title
