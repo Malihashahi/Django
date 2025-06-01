eab158fb441faff018f103c8ff49fea10bf880d1
Malihashahi <maliha.shahi.03@gmail.com>
2025-06-01 21:49:43 +0430

    adding static image

--- courses_app/models.py
+++ courses_app/models.py
@@ -5,7 +5,7 @@ class Course(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField()
     statution = models.BooleanField(default=True)
-    views = models.IntegerField()
+    views = models.IntegerField(default=0)
     image = models.ImageField(null=True , upload_to="codeyad")
 
 
